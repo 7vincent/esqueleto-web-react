8f9dd464edf2f674b79a435ab305975e9b356257
Vicente Albuquerque <7.vincent@gmail.com>
2020-03-09 23:01:49 -0300

    Add unform, config redux, layout auth

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um email válido!")
+    .required("Email é obrigatório"),
+  password: Yup.string().required("A Senha é obrogatória!"),
+
+});
 
 export default function SignIn() {
+
+
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
-    <h1>Oi SignIn</h1>
+    <>
+      <img src={logo} alt="" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="text" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta.</Link>
+      </Form>
+    </>
   );
 }
